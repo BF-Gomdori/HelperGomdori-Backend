58e041248e8003dde3b46e7147f223a984287b10
seungmin Ryu <ojysep9987@naver.com>
2022-08-10 18:28:26 +0900

    Prototype: 베리어프리 유저 관련 모듈

---
+++ bf-user/src/main/java/bf/multi/server/BfUserApplication.java
@@ -0,0 +1,11 @@
+package bf.multi.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BfUserApplication.class, args);
+    }
+}
