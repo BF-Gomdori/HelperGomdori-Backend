665fddec96a469fafba0d4464d7cdac970a7a9f0
mingeun0507 <mkkim999@gmail.com>
2022-08-12 02:05:57 +0900

    Fix: User 엔티티가 다시 UserDetail 구현하도록 변경

--- bf-user/src/main/java/bf/multi/server/domain/user/User.java
+++ bf-user/src/main/java/bf/multi/server/domain/user/User.java
@@ -2,18 +2,23 @@ package bf.multi.server.domain.user;
 
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
 @ToString
 @Entity
 @Table(name = "USER")
-public class User {
+public class User implements UserDetails {
 
     // UserDetails는 시큐리티가 관리하는 객체
     // ID, NAME, EMAIL, PHOTO_LINK, GENDER, PHONE, AGE, INTRO, START_DATE, MODIFIED_DATE
@@ -68,38 +73,38 @@ public class User {
         this.roles = roles;
     }
 
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
-//    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
 
-//    @Override
-//    public String getPassword() {
-//        return null;
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return name;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return true;
-//    }
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
