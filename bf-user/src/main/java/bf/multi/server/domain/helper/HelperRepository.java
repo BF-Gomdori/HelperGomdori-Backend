8fac6d0789bd81fcac406c2d723c1fdbd3b2d5c1
mingeun0507 <mkkim999@gmail.com>
2022-08-12 02:13:13 +0900

    Feat: HelperRepository.java 생성

---
+++ bf-user/src/main/java/bf/multi/server/domain/helper/HelperRepository.java
@@ -0,0 +1,7 @@
+package bf.multi.server.domain.helper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HelperRepository extends JpaRepository<Helper, Long> {
+
+}
