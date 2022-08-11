e4031250e66616224267755330235f9f46b8500f
mingeun0507 <mkkim999@gmail.com>
2022-08-12 01:49:21 +0900

    fix: CustomUserDetailService.java 다시 추가 및 오류 수정

---
+++ bf-user/src/main/java/bf/multi/server/service/CustomUserDetailService.java
@@ -0,0 +1,21 @@
+package bf.multi.server.service;
+
+import bf.multi.server.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return (UserDetails) userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+    }
+}
