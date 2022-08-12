17af3a8f465995e010a1dfaf518e7008db76ff17
mingeun0507 <mkkim999@gmail.com>
2022-08-12 13:41:44 +0900

    Feat: HelpeeRepository.java 생성

---
+++ bf-user/src/main/java/bf/multi/server/domain/helpee/HelpeeRepository.java
@@ -0,0 +1,7 @@
+package bf.multi.server.domain.helpee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HelpeeRepository extends JpaRepository<Helpee, Long> {
+
+}
