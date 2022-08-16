ca0da79eda999806d44c815fb1d201e86baffb61
mingeun0507 <mkkim999@gmail.com>
2022-08-16 11:38:25 +0700

    Feat: HelpeeRepository.java findByUser_Email() 메서드 추가

--- bf-user/src/main/java/bf/multi/server/domain/helpee/HelpeeRepository.java
+++ bf-user/src/main/java/bf/multi/server/domain/helpee/HelpeeRepository.java
@@ -2,6 +2,8 @@ package bf.multi.server.domain.helpee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HelpeeRepository extends JpaRepository<Helpee, Long> {
+import java.util.Optional;
 
+public interface HelpeeRepository extends JpaRepository<Helpee, Long> {
+    Optional<Helpee> findByUser_Email(String email);
 }
