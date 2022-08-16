6661ad6f8735e357ca25afc0f8cf3d11da75514e
mingeun0507 <mkkim999@gmail.com>
2022-08-16 11:09:14 +0700

    Test: HelperRepository.java findByUser_Email() 메서드 테스트

--- bf-user/src/test/java/bf/multi/server/domain/helper/HelperRepositoryTest.java
+++ bf-user/src/test/java/bf/multi/server/domain/helper/HelperRepositoryTest.java
@@ -23,7 +23,7 @@ class HelperRepositoryTest {
     @Test
     @DisplayName("헬퍼 정보가 잘 저장되고 잘 불러와질까요")
     public void 저장_불러오기() {
-        User user = userRepository.findByEmail("example@gmail.com").orElseThrow();
+        User user = userRepository.findByEmail("example@naver.com").orElseThrow();
         Integer helpCount = 11;
         Double averageRate = 3.5;
         Integer hearts = 72;
@@ -39,10 +39,21 @@ class HelperRepositoryTest {
 
         List<Helper> helperList = helperRepository.findAll();
 
-        Assertions.assertEquals(helperList.get(0).getAverageRate(), averageRate);
-        Assertions.assertEquals(helperList.get(0).getUser().getName(), user.getName());
-        Assertions.assertEquals(helperList.get(0).getId(), userRepository.
-                findByEmail("example@gmail.com").orElseThrow().
+        Assertions.assertEquals(helperList.get(1).getAverageRate(), averageRate);
+        Assertions.assertEquals(helperList.get(1).getUser().getName(), user.getName());
+        Assertions.assertEquals(helperList.get(1).getId(), userRepository.
+                findByEmail("example@naver.com").orElseThrow().
                 getHelper().getId());
+    }
+
+    @Test
+    @DisplayName("findByUser_Email() 메서드 작동 테스트임~")
+    public void 이메일로_찾기() {
+        Assertions.assertEquals(helperRepository
+                .findByUser_Email("example@gmail.com")
+                .orElseThrow().getId(), userRepository
+                .findByEmail("example@gmail.com")
+                .orElseThrow().getId()
+        );
     }
 }
