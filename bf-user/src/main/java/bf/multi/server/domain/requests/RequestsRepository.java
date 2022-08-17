040bb60263064be06cac264b53c2521127c50bef
mingeun0507 <mkkim999@gmail.com>
2022-08-17 09:19:24 +0700

    Feat: RequestsRepository.java User email로 조회하는 메서드 추가

--- bf-user/src/main/java/bf/multi/server/domain/requests/RequestsRepository.java
+++ bf-user/src/main/java/bf/multi/server/domain/requests/RequestsRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RequestsRepository extends JpaRepository<Requests, Long> {
-    List<Requests> findAllByHelpee_Id(Long id);
+    List<Requests> findAllByHelpee_User_Id(Long id);
+
+    List<Requests> findAllByHelpee_User_Email(String email);
 }
