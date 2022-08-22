85d6b342a59b974d07c49f33e571f826639bfc00
seungmin Ryu <ojysep9987@naver.com>
2022-08-23 02:29:04 +0900

    Feat: access-token받아서 JWT 발행 및 JWT로 유저정보 조회하기 마무리

--- bf-user/src/main/java/bf/multi/server/controller/AuthController.java
+++ bf-user/src/main/java/bf/multi/server/controller/AuthController.java
@@ -1,17 +1,17 @@
 package bf.multi.server.controller;
 
-import bf.multi.server.domain.dto.JwtTokenDto;
-import bf.multi.server.domain.dto.UserDto;
-import bf.multi.server.domain.dto.UserLoginDto;
-import bf.multi.server.domain.dto.UserSignUpDto;
+import bf.multi.server.domain.dto.*;
 import bf.multi.server.domain.user.User;
 import bf.multi.server.service.AuthService;
+import bf.multi.server.service.KakaoUserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.Objects;
 
@@ -19,7 +19,7 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-
+    private final KakaoUserService kakaoUserService;
     private final AuthService authService;
 
     @PostMapping("/login")
@@ -41,5 +41,11 @@ public class AuthController {
     public UserDto userSignUp(@Valid @RequestBody UserSignUpDto signUpDto) {
         User user = authService.registerUser(signUpDto);
         return UserDto.from(user);
+    }
+    @PostMapping("/barrierfree")
+    public User bfRegister(
+            @RequestBody KakaoLoginDto kakaoLoginDto,
+            HttpServletResponse response) throws JsonProcessingException {
+        return kakaoUserService.kakaoRegister(kakaoLoginDto, response);
     }
 }
--- bf-user/src/main/java/bf/multi/server/controller/UserController.java
+++ bf-user/src/main/java/bf/multi/server/controller/UserController.java
@@ -4,12 +4,14 @@ import bf.multi.server.domain.dto.UserDto;
 import bf.multi.server.domain.user.User;
 import bf.multi.server.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/users")
@@ -18,11 +20,12 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/self")
-    public UserDto userSelfDetail() {
+    public User userSelfDetail() {
         // TODO: SecurityContextHolder 에서 유저 정보 조회
         // https://docs.spring.io/spring-security/site/docs/4.2.x/reference/html/test-method.html
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        User user = userService.loadUserByUsername(userDetails.getUsername());
-        return UserDto.from(user);
+        log.info("username: "+userDetails.getUsername()+"password: "+userDetails.getPassword());
+        User user = userService.loadUserByEncodedEmail(userDetails.getPassword());
+        return user;
     }
 }
