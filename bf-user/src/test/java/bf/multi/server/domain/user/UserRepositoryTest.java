e55a398ae51e40d85a9e6bfd6aa9eb0ce75dcdea
mingeun0507 <mkkim999@gmail.com>
2022-08-23 15:26:40 +0900

    Test: Merge 후 UserRepositoryTest 정상 작동 확인

--- bf-user/src/test/java/bf/multi/server/domain/user/UserRepositoryTest.java
+++ bf-user/src/test/java/bf/multi/server/domain/user/UserRepositoryTest.java
@@ -22,29 +22,32 @@ class UserRepositoryTest {
     @DisplayName("유저 정보가 잘 저장되고 잘 불러와지는지 테스트~")
     public void 저장_불러오기() {
         String name = "admin";
-        String email = "admin@social.com";
-//        String photoLink = "www.example.com";
-//        String gender = "M";
-//        String phone = "01000000000";
-//        Integer age = 24;
+        String email = "admin2@social.com";
         String intro = "자기소개";
-//        Timestamp startDate = new Timestamp(System.currentTimeMillis());
-//        Timestamp modifiedDate = new Timestamp(System.currentTimeMillis());
-//
-//
-//        userRepository.save(
-//                User.builder()
-//                        .username(name)
-//                        .email(email)
-//                        .photoLink(photoLink)
-//                        .gender(gender)
-//                        .phone(phone)
-//                        .age(age)
-//                        .intro(intro)
-//                        .startDate(startDate)
-//                        .modifiedDate(modifiedDate)
-//                        .build()
-//        );
+        String password = "PW_admin2@social.com";
+
+        String photoLink = "www.example.com";
+        String gender = "M";
+        String phone = "01000000000";
+        Integer age = 24;
+        Timestamp startDate = new Timestamp(System.currentTimeMillis());
+        Timestamp modifiedDate = new Timestamp(System.currentTimeMillis());
+
+
+        userRepository.save(
+                User.builder()
+                        .username(name)
+                        .email(email)
+                        .password(password)
+                        .photoLink(photoLink)
+                        .gender(gender)
+                        .phone(phone)
+                        .age(age)
+                        .intro(intro)
+                        .startDate(startDate)
+                        .modifiedDate(modifiedDate)
+                        .build()
+        );
 
         List<User> userList = userRepository.findAll();
 
