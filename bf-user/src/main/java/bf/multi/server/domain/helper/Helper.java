eb9e2636b59c282a1618cf2c68d85deb0fd5b572
mingeun0507 <mkkim999@gmail.com>
2022-08-23 11:03:23 +0900

    Feat: Helper에서 Helps 조회를 위한 양방향 키 추가

--- bf-user/src/main/java/bf/multi/server/domain/helper/Helper.java
+++ bf-user/src/main/java/bf/multi/server/domain/helper/Helper.java
@@ -1,5 +1,6 @@
 package bf.multi.server.domain.helper;
 
+import bf.multi.server.domain.helps.Helps;
 import bf.multi.server.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -35,6 +38,10 @@ public class Helper {
 
     @Column(name = "HEARTS", nullable = false)
     private Integer hearts;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "helper")
+    private List<Helps> helpsList = new ArrayList<>();
 
     @Builder
     public Helper(User user, Integer helpCount, Double averageRate, Integer hearts) {
