5a7a8b7280ef5a87a947ab9538ab0eb19d281b66
mingeun0507 <mkkim999@gmail.com>
2022-08-24 11:46:58 +0900

    Refactor: UserDto 코드 구조 변경

--- bf-user/src/main/java/bf/multi/server/domain/dto/user/UserDto.java
+++ bf-user/src/main/java/bf/multi/server/domain/dto/user/UserDto.java
@@ -1,13 +1,12 @@
 package bf.multi.server.domain.dto.user;
 
 import bf.multi.server.domain.user.User;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
-@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
 
     private String username;
@@ -17,5 +16,11 @@ public class UserDto {
     public static UserDto from(User user) {
         // Entity to DTO
         return new UserDto((user.getUsername()), user.getEmail());
+    }
+
+    @Builder
+    public UserDto(String username, String email) {
+        this.username = username;
+        this.email = email;
     }
 }
