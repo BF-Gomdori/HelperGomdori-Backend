b25620aaa18a8adc68e42f7c78983ff6e4a2dda4
seungmin Ryu <ojysep9987@naver.com>
2022-08-27 01:54:51 +0900

    handler 처리 로직 진행 중

--- bf-user/src/main/java/bf/multi/server/websocket/handler/StompHandler.java
+++ bf-user/src/main/java/bf/multi/server/websocket/handler/StompHandler.java
@@ -2,6 +2,7 @@ package bf.multi.server.websocket.handler;
 
 import bf.multi.server.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
@@ -12,16 +13,22 @@ import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class StompHandler implements ChannelInterceptor {
 
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        log.info("WebSocket connect : "+String.valueOf(message));
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
         if(accessor.getCommand() == StompCommand.CONNECT) {
             if(!jwtTokenProvider.validateToken(accessor.getFirstNativeHeader("Authorization")))
                 throw new AccessDeniedException("");
+//            else if(StompCommand.SUBSCRIBE == accessor.getCommand()){ // 앱에 접속하거나 도움 요청을 수락했을 때
+//                String bearerToken = accessor.getFirstNativeHeader("Authorization");
+//                log.info("WebSocket Connector : "+jwtTokenProvider.getUsernameByToken(bearerToken));
+//            }
         }
         return message;
     }
