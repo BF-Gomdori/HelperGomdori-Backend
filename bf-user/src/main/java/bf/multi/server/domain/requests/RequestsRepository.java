2ed8dc2053bd54c4ef2520d19dfea44016bfed76
seungmin Ryu <ojysep9987@naver.com>
2022-08-28 16:34:27 +0900

    Feat: repo 수정

--- bf-user/src/main/java/bf/multi/server/domain/requests/RequestsRepository.java
+++ bf-user/src/main/java/bf/multi/server/domain/requests/RequestsRepository.java
@@ -1,11 +1,15 @@
 package bf.multi.server.domain.requests;
 
+import bf.multi.server.domain.helpee.Helpee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RequestsRepository extends JpaRepository<Requests, Long> {
     List<Requests> findAllByHelpee_User_Id(Long id);
 
-    List<Requests> findAllByHelpee_User_Email(String email);
+//    List<Requests> findAllByHelpee_User_Email(String email);
+    Optional<Requests> findAllByHelpee_User_Email(String email);
+    Requests findDistinctFirstByHelpee(Helpee helpee);
 }
