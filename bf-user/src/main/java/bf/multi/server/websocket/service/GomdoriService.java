aa4fc911c1adc9ccef7e8e0939e697d2cf285769
mingeun0507 <mkkim999@gmail.com>
2022-08-30 00:42:37 +0900

    Fix: 컬럼명 변경에 따른 수정

--- bf-user/src/main/java/bf/multi/server/websocket/service/GomdoriService.java
+++ bf-user/src/main/java/bf/multi/server/websocket/service/GomdoriService.java
@@ -51,8 +51,8 @@ public class GomdoriService {
         Requests requests = Requests.builder()
                 .helpee(helpee)
                 .complete(false)
-                .reqType(messageDto.getHelpRequestDto().getRequestType())
-                .reqDetail(messageDto.getHelpRequestDto().getRequestDetail())
+                .requestType(messageDto.getHelpRequestDto().getRequestType())
+                .requestDetail(messageDto.getHelpRequestDto().getRequestDetail())
                 .location(messageDto.getHelpRequestDto().getDetailLocation())
                 .requestTime(new Timestamp(System.currentTimeMillis()))
                 .build();
