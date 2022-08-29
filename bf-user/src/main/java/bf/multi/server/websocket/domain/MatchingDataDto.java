315f0812cc83766e7c5f17203d2fc3f5efbf779a
seungmin Ryu <ojysep9987@naver.com>
2022-08-30 01:42:45 +0900

    Feat: 매칭되었을때 response ping DTO

---
+++ bf-user/src/main/java/bf/multi/server/websocket/domain/MatchingDataDto.java
@@ -0,0 +1,46 @@
+package bf.multi.server.websocket.domain;
+
+import bf.multi.server.domain.helps.Helps;
+import bf.multi.server.domain.requests.Requests;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MatchingDataDto {
+
+    private HelperLocation helperLocation;
+    private HelpeeLocation helpeeLocation;
+
+    @Data
+    @NoArgsConstructor
+    public static class HelperLocation{
+        private String helperX;
+        private String helperY;
+
+        @Builder
+        public HelperLocation(String helperX, String helperY) {
+            this.helperX = helperX;
+            this.helperY = helperY;
+        }
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class HelpeeLocation{
+        private String helpeeX;
+        private String helpeeY;
+
+        @Builder
+        public HelpeeLocation(String helpeeX, String helpeeY) {
+            this.helpeeX = helpeeX;
+            this.helpeeY = helpeeY;
+        }
+    }
+    @Builder
+    public MatchingDataDto(HelperLocation helperLocation, HelpeeLocation helpeeLocation) {
+        this.helperLocation = helperLocation;
+        this.helpeeLocation = helpeeLocation;
+    }
+}
