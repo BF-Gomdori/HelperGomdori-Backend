26b0970281df429c59a4e3573f71ca42c2c4298d
seungmin Ryu <ojysep9987@naver.com>
2022-08-29 16:57:52 +0900

    Feat: 베프, 곰돌이 접속자 수 통계 api

--- bf-user/src/main/java/bf/multi/server/controller/ApiController.java
+++ bf-user/src/main/java/bf/multi/server/controller/ApiController.java
@@ -2,6 +2,7 @@ package bf.multi.server.controller;
 
 import bf.multi.server.domain.helpee.Helpee;
 import bf.multi.server.domain.helper.Helper;
+import bf.multi.server.domain.helps.HelpsRepository;
 import bf.multi.server.domain.requests.Requests;
 import bf.multi.server.domain.requests.RequestsRepository;
 import bf.multi.server.domain.user.User;
@@ -10,13 +11,19 @@ import bf.multi.server.websocket.domain.HelpRequestDto;
 import bf.multi.server.websocket.domain.HelpeePingDto;
 import bf.multi.server.websocket.domain.HelperPingDto;
 import io.swagger.annotations.Api;
+import lombok.Builder;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.config.WebSocketMessageBrokerStats;
+
+import javax.websocket.WebSocketContainer;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -25,6 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 
     private final UserService userService;
+    private final HelpsRepository helpsRepository;
     private final RequestsRepository requestsRepository;
 
     // 베프의 핑을 눌렀을 때 보이는 정보
@@ -53,6 +61,24 @@ public class ApiController {
                         .requestType(requests.getReqType())
                         .build())
                 .build();
+    }
+
+    // 현재 접속자 수
+    @GetMapping("/connect/users")
+    public UserNum countBfAndGomdori(){
+        long bf = helpsRepository.findAllBySuccessIsFalse().stream().count();
+        long gomdori = requestsRepository.findAllByCompleteIsFalse().stream().count();
+        return UserNum.builder().bf(bf).gomdori(gomdori).build();
+    }
+    @Data
+    static class UserNum{
+        long bf;
+        long gomdori;
+        @Builder
+        public UserNum(long bf, long gomdori) {
+            this.bf = bf;
+            this.gomdori = gomdori;
+        }
     }
 
 }
