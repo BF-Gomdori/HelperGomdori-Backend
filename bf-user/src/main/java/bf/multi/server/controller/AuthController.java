19c43c53222ad34b45a9da94540e189b4edbbddb
mingeun0507 <mkkim999@gmail.com>
2022-08-29 12:19:44 +0900

    Feat: helperSignup API 추가

--- bf-user/src/main/java/bf/multi/server/controller/AuthController.java
+++ bf-user/src/main/java/bf/multi/server/controller/AuthController.java
@@ -2,8 +2,11 @@ package bf.multi.server.controller;
 
 import bf.multi.server.domain.dto.helpee.HelpeeResponseDto;
 import bf.multi.server.domain.dto.helpee.HelpeeSignUpDto;
+import bf.multi.server.domain.dto.helper.HelperResponseDto;
+import bf.multi.server.domain.dto.helper.HelperSignUpDto;
 import bf.multi.server.domain.dto.user.*;
 import bf.multi.server.domain.helpee.Helpee;
+import bf.multi.server.domain.helper.Helper;
 import bf.multi.server.domain.user.User;
 import bf.multi.server.service.AuthService;
 import bf.multi.server.service.KakaoUserService;
@@ -19,7 +22,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -63,13 +65,24 @@ public class AuthController {
 
     @PostMapping("/signup/helpee")
     public HelpeeResponseDto helpeeSignup(
-            @RequestBody HelpeeSignUpDto helpeeSignUpDto) {
-
+            @RequestBody String type) {
+        HelpeeSignUpDto helpeeSignUpDto = HelpeeSignUpDto.builder().type(type).build();
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         helpeeSignUpDto.setUser(authService.getUserByPassword(userDetails.getPassword()));
         Helpee helpee = authService.connectHelpee(helpeeSignUpDto);
 
         return HelpeeResponseDto.from(helpee);
+    }
+
+
+    @PostMapping("/signup/helper")
+    public HelperResponseDto helperSignup() {
+        HelperSignUpDto helperSignUpDto = new HelperSignUpDto();
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        helperSignUpDto.setUser(authService.getUserByPassword(userDetails.getPassword()));
+        Helper helper = authService.connectHelper(helperSignUpDto);
+
+        return HelperResponseDto.from(helper);
     }
 
 }
