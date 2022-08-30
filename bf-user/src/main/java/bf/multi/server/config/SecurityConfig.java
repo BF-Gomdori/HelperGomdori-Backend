637b8a3ab4f7a0eaf625dded712c8f7a3850824e
seungmin Ryu <ojysep9987@naver.com>
2022-08-31 02:11:04 +0900

    Feat: api -> websocket 통신 권한 해제

--- bf-user/src/main/java/bf/multi/server/config/SecurityConfig.java
+++ bf-user/src/main/java/bf/multi/server/config/SecurityConfig.java
@@ -86,6 +86,7 @@ public class SecurityConfig {
                 .antMatchers(
                         "/",
                         "/api/connect/users",
+                        "/send",
                         "/dori/**", // websocket
                         "/v2/api-docs",
                         "/swagger-resources/**",
