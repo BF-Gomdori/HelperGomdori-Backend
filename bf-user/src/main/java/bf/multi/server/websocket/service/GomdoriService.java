a6c652d5065a010f62da162e6b7707d57db3a71c
seungmin Ryu <ojysep9987@naver.com>
2022-08-31 02:20:39 +0900

    Feat: 베어프렌즈와 곰돌이가 매칭되었을 때 business logic 구현 추가

--- bf-user/src/main/java/bf/multi/server/websocket/service/GomdoriService.java
+++ bf-user/src/main/java/bf/multi/server/websocket/service/GomdoriService.java
@@ -29,6 +29,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class GomdoriService {
     private final JwtTokenProvider jwtTokenProvider;
+    private final FindConnectedUsersService findConnectedUsersService;
     private final UserService userService;
     private final RequestsService requestsService;
     private final UserRepository userRepository;
@@ -40,23 +41,21 @@ public class GomdoriService {
 
     // 현재 자기 위치 정보 전송
     public void sendMessage(MessageDto messageDto){ // 메세지 타입에 따라 동작 구분
-        messageDto.setTime(new Timestamp(System.currentTimeMillis()));
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        messageDto.setTime(now);
         if(MessageDto.MessageType.ENTER.equals(messageDto.getType())){ // 위치 정보 표시할 때
-            // Helper 들어올 때 : 위치 정보, jwt, 접속 시간
+            // helps 생성 + 입장 메세지 전송
             createHelp(messageDto); // help default 생성
-            messageDto.setHelpeeJwt(null);
             messageDto.setHelpRequest(null);
             simpMessageSendingOperations.convertAndSend("/map/"+ messageDto.getSub(), messageDto);
         }else if(MessageDto.MessageType.QUIT.equals(messageDto.getType())){ // 위치 정보 표시 안 할 때
             // TODO: 뭐해줘야되지?
         }else if(MessageDto.MessageType.HELP.equals(messageDto.getType())){ // 도움 요청 할 때
-            // TODO: 요청사항 저장 및 + ?
+            // 요청사항 저장 및 + 도움 요청 메세지 전송
             createRequests(messageDto); // request default 생성
             // 메인 화면에 정보 뿌리기
-            messageDto.setHelperJwt(null);
             simpMessageSendingOperations.convertAndSend("/map/"+ messageDto.getSub(), messageDto);
         }else if(MessageDto.MessageType.ACCEPT.equals(messageDto.getType())){ // 도움 수락 할 때
-            // TODO: 베:프 구독 변경 및 위치 정보 재전송
             simpMessageSendingOperations.convertAndSend("/map/"+ messageDto.getSub(), messageDto);
         }
     }
@@ -64,11 +63,11 @@ public class GomdoriService {
     // 베프가 들어오면 helps 객체 하나 생성
     @Transactional
     public void createHelp(MessageDto messageDto){
-        Helper helper = helperRepository.findHelperByUser_Username(jwtTokenProvider.getUsernameByToken(messageDto.getHelperJwt()));
+        Helper helper = helperRepository.findHelperByUser_Username(jwtTokenProvider.getUsernameByToken(messageDto.getJwt()));
         Helps helps = Helps.builder()
                 .helper(helper).requests(null)
                 .x(messageDto.getLocation().getX()).y(messageDto.getLocation().getY())
-                .acceptTime(null).finishTime(null)
+                .acceptTime(messageDto.getTime()).finishTime(null)
                 .success(false)
                 .helperRate(2.5).helpeeRate(2.5)
                 .helperMessage(null).helpeeMessage(null)
@@ -80,7 +79,8 @@ public class GomdoriService {
     // 곰돌이가 도움 요청할 때 채팅방 하나 따로 생성
     @Transactional
     public void createRequests(MessageDto messageDto) {
-        Helpee helpee = helpeeRepository.findByUser_Username(jwtTokenProvider.getUsernameByToken(messageDto.getHelpeeJwt()));
+        Helpee helpee = helpeeRepository.findByUser_Username(
+                jwtTokenProvider.getUsernameByToken(messageDto.getHelpRequest().getHelpeeJwt()));
         Requests requests = Requests.builder()
                 .helpee(helpee).complete(false)
                 .requestType(messageDto.getHelpRequest().getRequestType())
@@ -88,11 +88,7 @@ public class GomdoriService {
                 .location(messageDto.getHelpRequest().getDetailLocation())
                 .x(messageDto.getLocation().getX()).y(messageDto.getLocation().getY())
                 .helpee(helpee)
-                .complete(false)
-                .requestType(messageDto.getHelpRequest().getRequestType())
-                .requestDetail(messageDto.getHelpRequest().getRequestDetail())
-                .location(messageDto.getHelpRequest().getDetailLocation())
-                .requestTime(new Timestamp(System.currentTimeMillis()))
+                .requestTime(messageDto.getTime())
                 .build();
         requestsRepository.save(requests);
     }
@@ -133,7 +129,7 @@ public class GomdoriService {
             Optional<Helper> helper = helperRepository.findHelperByUser_Email(user1.get().getEmail());
             Helps helps = helpsRepository.findDistinctFirstByHelperOrderByAcceptTimeDesc(helper);
             Helpee helpee = helpeeRepository.findByUser_Username(user2.get().getUsername());
-            Requests requests = requestsRepository.findDistinctFirstByHelpeeAndCompleteIsFalse(helpee);
+            Requests requests = requestsRepository.findDistinctTopByHelpeeOrderByRequestTimeDesc(helpee);
             return MatchingDataDto.builder()
                     .helperLocation(MatchingDataDto.HelperLocation.builder().helperX(helps.getX()).helperY(helps.getY()).build())
                     .helpeeLocation(MatchingDataDto.HelpeeLocation.builder().helpeeX(requests.getX()).helpeeY(requests.getY()).build())
@@ -142,13 +138,27 @@ public class GomdoriService {
         Optional<Helper> helper = helperRepository.findHelperByUser_Email(user2.get().getEmail());
         Helps helps = helpsRepository.findDistinctFirstByHelperOrderByAcceptTimeDesc(helper);
         Helpee helpee = helpeeRepository.findByUser_Username(user1.get().getUsername());
-        Requests requests = requestsRepository.findDistinctFirstByHelpeeAndCompleteIsFalse(helpee);
+        Requests requests = requestsRepository.findDistinctTopByHelpeeOrderByRequestTimeDesc(helpee);
         return MatchingDataDto.builder()
                 .helperLocation(MatchingDataDto.HelperLocation.builder().helperX(helps.getX()).helperY(helps.getY()).build())
                 .helpeeLocation(MatchingDataDto.HelpeeLocation.builder().helpeeX(requests.getX()).helpeeY(requests.getY()).build())
                 .build();
     }
 
-    // 앱에 접속할 때 /sub/ch/main을 구독하도록함
-    // 앱이 종료될 때 main 방에서 나가도록함, [곰돌이:n명 | 베프:n명] 표시
+    // accept Message 왔을 때 helps, requests 업데이트
+    @Transactional
+    public void updateHelpsAndRequests(MessageDto messageDto){
+        Optional<User> helperUser = userRepository.findByUsername(
+                jwtTokenProvider.getUsernameByToken(messageDto.getJwt()));
+        Optional<User> helpeeUser = userRepository.findByUsername(
+                jwtTokenProvider.getUsernameByToken(messageDto.getHelpRequest().getHelpeeJwt()));
+        Optional<Helper> helper = helperRepository.findHelperByUser_Email(helperUser.get().getEmail());
+        Helpee helpee = helpeeRepository.findByUser_Username(helpeeUser.get().getUsername());
+
+        Requests requests = requestsRepository.findDistinctTopByHelpeeOrderByRequestTimeDesc(helpee);
+        Helps helps = helpsRepository.findDistinctFirstByHelperOrderByAcceptTimeDesc(helper);
+        requests.updateRequests(messageDto);
+        helps.updateHelps(messageDto, requests);
+    }
+
 }
