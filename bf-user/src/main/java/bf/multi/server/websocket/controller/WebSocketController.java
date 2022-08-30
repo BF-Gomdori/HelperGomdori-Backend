a5cc9a79c2e2a741ac1bc13fe32dd23b2bfce9d3
seungmin Ryu <ojysep9987@naver.com>
2022-08-31 02:19:25 +0900

    Feat: 베어프렌즈와 곰돌이가 매칭되었을 때 logic 추가

--- bf-user/src/main/java/bf/multi/server/websocket/controller/WebSocketController.java
+++ bf-user/src/main/java/bf/multi/server/websocket/controller/WebSocketController.java
@@ -23,10 +23,10 @@ public class WebSocketController {
     private final JwtTokenProvider jwtTokenProvider;
 
     // 자신의 위치 정보 표시한다고 눌렀을 때
-    @MessageMapping("/enter")
+    @MessageMapping("/connecting")
     public void initEnter(MessageDto messageDto) {
         if(messageDto.getType().equals(MessageDto.MessageType.ENTER)) {
-            String username = jwtTokenProvider.getUsernameByToken(messageDto.getHelperJwt());
+            String username = jwtTokenProvider.getUsernameByToken(messageDto.getJwt());
             User user = userService.loadUserByUsername(username);
             log.info("============= 베:프 현재 위치 전송 =============");
             log.info("[" + new Timestamp(System.currentTimeMillis()) + "] username: "
@@ -36,18 +36,15 @@ public class WebSocketController {
         }
         else if(messageDto.getType().equals(MessageDto.MessageType.HELP)){
             log.info("============= 곰돌이 현재 위치 전송 =============");
-            log.info("도움 요청서 : " + messageDto.getHelpRequest());
+            log.info("도움 요청서 : " + messageDto.getHelpRequest().toString());
             gomdoriService.sendMessage(messageDto);
         }
-    }
-
-    // 곰돌이가 도움 요청할 때 방 생성
-    @MessageMapping("/help") // 메인 맵에다가 자신의 위치 및 도움 필요 정보 뿌림
-    public void reqHelp(MessageDto messageDto){
-        log.info("========== 곰돌이 도움 요청 !!! ==========");
-//        HelpMessage help = gomdoriService.createHelp(helpMessage);
-//        log.info("[도움 요청 정보] : "+help);
-        gomdoriService.sendMessage(messageDto);
+        else if(messageDto.getType().equals(MessageDto.MessageType.ACCEPT)){
+            log.info("============= 곰돌이 & 베:프 매칭 =============");
+            gomdoriService.sendMessage(messageDto);
+            gomdoriService.updateHelpsAndRequests(messageDto);
+            // TODO: 매칭 되고 모든 유저에게 다시 메세지 전송해줘야하나?
+        }
     }
 
 }
