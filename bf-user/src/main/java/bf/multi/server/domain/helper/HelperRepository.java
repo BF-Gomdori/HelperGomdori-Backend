3b5a83c22761feb8ed5eba5d5840913e6fd28f91
seungmin Ryu <ojysep9987@naver.com>
2022-08-31 15:55:53 +0900

    Chore: 오타 수정

--- bf-user/src/main/java/bf/multi/server/domain/helper/HelperRepository.java
+++ bf-user/src/main/java/bf/multi/server/domain/helper/HelperRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 public interface HelperRepository extends JpaRepository<Helper, Long> {
     Optional<Helper> findByUser_Email(String email);
     Optional<Helper> findHelperByUser_Email(String email);
-    Helper findHelperByUser_Username(String username);
+    Optional<Helper> findHelperByUser_Username(String username);
 }
