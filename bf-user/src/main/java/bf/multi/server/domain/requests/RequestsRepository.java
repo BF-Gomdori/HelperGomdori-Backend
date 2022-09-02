de8c8006652f3c81e6432f3d2b24685075c0ec14
mingeun0507 <mkkim999@gmail.com>
2022-09-02 18:30:55 +0900

    Feat: Requests 삭제 메서드 생성

--- bf-user/src/main/java/bf/multi/server/domain/requests/RequestsRepository.java
+++ bf-user/src/main/java/bf/multi/server/domain/requests/RequestsRepository.java
@@ -11,13 +11,19 @@ public interface RequestsRepository extends JpaRepository<Requests, Long> {
     List<Requests> findAllByHelpee_User_Id(Long id);
 
 //    List<Requests> findAllByHelpee_User_Email(String email);
-    Optional<Requests> findAllByHelpee_User_Email(String email);
+Optional<Requests> findAllByHelpee_User_Email(String email);
+
     Requests findDistinctFirstByHelpeeAndCompleteIsFalse(Helpee helpee);
+
     Requests findDistinctTopByHelpeeOrderByRequestTimeDesc(Helpee helpee);
+
     List<Requests> findAllByCompleteIsFalse();
+
     List<Requests> findAllByCompleteIsFalseAndRequestTimeBefore(Timestamp timestamp);
 
     void deleteByHelpee_User_Email(String email);
 
     Requests findDistinctTopByCompleteIsFalseAndHelpee_User_UsernameOrderByRequestTimeDesc(String username);
+
+    void deleteAllByHelpee_User_Email(String email);
 }
