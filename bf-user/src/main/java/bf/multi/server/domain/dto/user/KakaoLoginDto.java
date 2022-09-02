28c0b8a16ee9c19d82e1f51eb7c4f664229bf7e7
mingeun0507 <mkkim999@gmail.com>
2022-09-02 17:36:48 +0900

    Feat: Entity 칼럼 추가에 따른 DTO 변경

--- bf-user/src/main/java/bf/multi/server/domain/dto/user/KakaoLoginDto.java
+++ bf-user/src/main/java/bf/multi/server/domain/dto/user/KakaoLoginDto.java
@@ -10,13 +10,14 @@ import lombok.NoArgsConstructor;
 public class KakaoLoginDto {
     @JsonProperty("access_token")
     private String accessToken;
-
+    private String FCMToken;
     private String phone;
     private String name;
 
     @Builder
-    public KakaoLoginDto(String accessToken, String phone, String name) {
+    public KakaoLoginDto(String accessToken, String FCMToken, String phone, String name) {
         this.accessToken = accessToken;
+        this.FCMToken = FCMToken;
         this.phone = phone;
         this.name = name;
     }
